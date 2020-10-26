df7af6a0d92a13940730ff4dc536f929b65f9035
Michael Demmer <mdemmer@slack-corp.com>
2020-10-25 17:30:52 -0700

    break out libio

--- src/c_hello.c
+++ src/c_hello.c
@@ -1,43 +1,5 @@
-#define KBV 060     // keyboard (TTI) interrupt vector
-#define KBS 0177560 // keyboard status register
-#define KBD 0177562 // keyboard data buffer
-#define KBR 0x80    // keyboard ready bit
 
-#define TTV 064     // terminal (TTO) interrupt vector
-#define TTS 0177564 // terminal status register
-#define TTD 0177566 // terminal data buffer
-#define TTR 0x80    // terminal ready bit
-
-#define CR 015
-#define LF 012
-
-char readchr()
-{
-    volatile unsigned int *xcsr = (unsigned int *)KBS;
-    unsigned char *xbuf = (unsigned char *)KBD;
-    while (!(*xcsr & KBR))
-        ;
-    char c = *xbuf;
-    return c;
-}
-
-void writechr(char c)
-{
-    volatile unsigned int *xcsr = (unsigned int *)TTS;
-    unsigned char *xbuf = (unsigned char *)TTD;
-    while (!(*xcsr & TTR))
-        ;
-    *xbuf = c;
-}
-
-void writestr(char *s)
-{
-    char *c = s;
-    while (*c != 0)
-    {
-        writechr(*c++);
-    }
-}
+#include "libio.h"
 
 void hello()
 {
---
+++ src/libio.h
@@ -0,0 +1,7 @@
+
+#define CR 015
+#define LF 012
+
+char readchr();
+void writechr(char c);
+void writestr(char *s);
